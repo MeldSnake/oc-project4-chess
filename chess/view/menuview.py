396351278aea20d314291b7d052a3f860bbb4a7d
MeldSnake <ikeek.33@gmail.com>
2022-12-18 15:43:28 +0100

    Ajout de vue pour les raports.
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- chess/view/menuview.py
+++ chess/view/menuview.py
@@ -1,4 +1,5 @@
 import datetime
+from chess.models.tournament import StyleTournament
 from chess.view.view import View
 
 
@@ -27,6 +28,87 @@ class PlayerReportView(View):
         if self.index is not None:
             print("%d) " % self.index, end='')
         print("Classement(%d)" % self.rank, self.first_name, self.last_name, "%d ans" % self.age, self.gender)
+
+
+class TournamentReportView(View):
+    def __init__(self, /,
+                 index: int | None = None,
+                 name="",
+                 where="",
+                 when: datetime.date | None = None,
+                 style=StyleTournament.BULLET,
+                 finished=False) -> None:
+        super().__init__()
+        self.index = index
+        self.name = name
+        self.where = where
+        self.when = when
+        self.style = style
+        self.finished = finished
+
+    def render(self):
+        if self.index is not None:
+            print("%d) " % self.index, end='')
+        print("Tournoi: `%s`" % self.name, end='')
+        if self.where != "":
+            print(" à `%s`" % self.where, end='')
+        if self.when is not None:
+            print(" le `%s`" % self.when.strftime("%d/%m/%Y"), end='')
+        print(", style: ", end='')
+        if self.style == StyleTournament.BULLET:
+            print("Bullet", end='')
+        elif self.style == StyleTournament.BLITZ:
+            print("Blitz", end='')
+        elif self.style == StyleTournament.FAST_STRIKE:
+            print("Coup rapide", end='')
+        if self.finished:
+            print(", [Terminé]", end='')
+        else:
+            print(", [En cours]", end='')
+        print()
+
+
+class RoundReportView(View):
+    def __init__(self, /,
+                 index: int | None = None,
+                 name="",
+                 number=-1) -> None:
+        super().__init__()
+        self.index = index
+        self.name = name
+        self.number = number
+
+    def render(self):
+        if self.index is not None:
+            print("%d) " % self.index, end='')
+        print("Ronde %d: `%s`" % (self.number, self.name))
+
+
+class MatchReportView(View):
+    def __init__(self, /,
+                 index: int | None = None,
+                 start_time: datetime.datetime | None = None,
+                 end_time: datetime.datetime | None = None,
+                 scores: tuple[float, float] = (0.0, 0.0)) -> None:
+        super().__init__()
+        self.index = index
+        self.start_time = start_time
+        self.end_time = end_time
+        self.scores = scores
+
+    def render(self):
+        if self.index is not None:
+            print("%d) " % self.index, end='')
+        print("Match %d:" % (self.index), end=' ')
+        if self.start_time is not None:
+            print(self.start_time.strftime("%d/%m/%Y %H:%M"), end='')
+        if self.end_time is not None:
+            print("-", self.end_time.strftime("%d/%m/%Y %H:%M"), end='')
+        if self.scores[0] == self.scores[1] and self.scores[0] == 0.0:
+            print("(%f, %f), [En cours]" % self.scores, end='')
+        else:
+            print("(%f, %f), [Terminé]" % self.scores, end='')
+        print()
 
 
 class MenuItemsView(View):
