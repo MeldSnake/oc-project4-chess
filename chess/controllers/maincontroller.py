cd37105d7a9923e825701c888358d7ca1a6b4371
MeldSnake <ikeek.33@gmail.com>
2022-12-23 15:17:55 +0100

    Ajout de documentation sur MainController
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- chess/controllers/maincontroller.py
+++ chess/controllers/maincontroller.py
@@ -1,26 +1,29 @@
 from typing import Any, Type
-from tinydb import TinyDB
 from chess.controllers.controller import Controller
 from chess.controllers.mainstate import MainViewState
 import chess.controllers.menucontrollers as mc
 import chess.controllers.menueditcontrollers as mec
 import chess.controllers.editcontrollers as ec
 from chess.controllers.reportcontrollers import ReportPlayersController, ReportRoundsController, ReportTournamentsController, ReportsMatchsController
+from chess.database.dbadapter import DBAdapter
 from chess.models.match import Match
 from chess.models.player import Player
 from chess.models.round import Round
 from chess.models.tournament import Tournament
+import copy
 
 
 class MainController(Controller):
+    """Main class handling the logic behind the root View."""
 
     @property
     def current_controller(self):
+        """Current controller being or to be displayed"""
         if len(self.previous_controllers) == 0:
             return None
         return self.previous_controllers[-1]
 
-    def __init__(self, db: TinyDB):
+    def __init__(self, db: DBAdapter):
         self._db = db
         self.players: list[Player] = []
         self.tournaments: list[Tournament] = []
@@ -38,6 +41,7 @@ class MainController(Controller):
         self.edited_type: Type[Any] | None
 
     def _clearFields(self):
+        """Clear all data within the controller, reseting it to default values"""
         self.tournaments.clear()
         self.players.clear()
         self.rounds.clear()
@@ -52,22 +56,25 @@ class MainController(Controller):
         self.edited_field = None
 
     def onLoadDatabase(self):
+        """Called upon requesting a database load, this also reset the states to the MAIN_MENU"""
         self._clearFields()
 
-        self.players = Player.all(self._db)
-        self.tournaments = Tournament.all(self._db)
-        self.rounds = Round.all(self._db)
-        self.matchs = Match.all(self._db)
+        self.players = list(self._db.all(Player))
+        self.tournaments = list(self._db.all(Tournament))
+        self.rounds = list(self._db.all(Round))
+        self.matchs = list(self._db.all(Match))
 
         self.states.append(MainViewState.MAIN_MENU)
 
     def onSaveDatabase(self, *_):
-        Player.save(self._db, *self.players)
-        Tournament.save(self._db, *self.tournaments)
-        Match.save(self._db, *self.matchs)
-        Round.save(self._db, *self.rounds)
+        """Called upon requesting a database save"""
+        self._db.save(*self.players)
+        self._db.save(*self.tournaments)
+        self._db.save(*self.rounds)
+        self._db.save(*self.matchs)
 
     def run(self):
+        """Main loop that runs all sub controllers and contains the root logic"""
         self.onLoadDatabase()
         self.previous_controllers: list[Controller] = []
         while len(self.states) > 0:
@@ -81,6 +88,7 @@ class MainController(Controller):
                 self.states.append(new_state)
 
     def _save_item(self):
+        """SAVE_ITEM: Save the currently edited item on the database depending on the state that requested it"""
         self.states.pop()
         old_state = self.states.pop()
         if old_state == MainViewState.EDIT_PLAYER:
@@ -105,31 +113,37 @@ class MainController(Controller):
                 self.current_match.update(self.edited_data)  # type: ignore
 
     def _unsupported(self):
-        print("DEBUG: Etat non supporté", self.states[-1])
+        """Default behaviour when an unknown/non-implemented state """
+        assert False, "Etat non supporté"
         self.states.pop()
         return None
 
     def _quit(self):
+        """QUIT: Reset the controller to its default values"""
         self._clearFields()
         return None
 
     def _save_database(self):
+        """SAVE_DATABASE: save all items onto the database"""
         self.onSaveDatabase()
         self.states.pop()
         return None
 
     def _load_database(self):
+        """LOAD_DATABASE: resets the controller and load all data from the database, ignoring pending changes"""
         self.onLoadDatabase()
         self.states.pop()
         return None
 
     def _back(self):
+        """BACK: Return back to the previous state"""
         self.states.pop()
         self.states.pop()
         self.previous_controllers.pop()
         return None
 
     def _edit_field(self):
+        """EDIT_FIELD: render the edit view for the current field being edited on the current item"""
         current_controller = self.current_controller
         if self.edited_field is None or self.edited_data is None:
             self.states.pop()
@@ -150,19 +164,20 @@ class MainController(Controller):
             return new_state
         return None
 
-    def _reports_match_menu(self):
+    def _reports_matchs_menu(self):
+        """REPORTS_MATCHS_MENU: Report all matchs from the current round (or all if None currently set)"""
         current_controller = self.current_controller
         if not isinstance(current_controller, ReportsMatchsController):
-            if self.current_tournament is not None:
-                # TODO Obtain all matches from current_round
-                current_controller = ReportRoundsController()
+            if self.current_round is not None:
+                current_controller = ReportsMatchsController(*[r for r in self.matchs if r.round is self.current_round])
             else:
-                current_controller = ReportRoundsController(*self.rounds)
+                current_controller = ReportsMatchsController(*self.matchs)
             self.previous_controllers.append(current_controller)
         new_state, _ = current_controller.run()
         return new_state
 
     def _reports_rounds_menu(self):
+        """REPORTS_ROUNDS_MENU: Report all rounds and query which one to report matchs about."""
         current_controller = self.current_controller
         if not isinstance(current_controller, ReportRoundsController):
             if self.current_tournament is not None:
@@ -179,6 +194,7 @@ class MainController(Controller):
         return new_state
 
     def _reports_tournaments_menu(self):
+        """REPORTS_TOURNAMENTS_MENU: Report all tournaments and query which one to report rounds about."""
         current_controller = self.current_controller
         if not isinstance(current_controller, ReportTournamentsController):
             current_controller = ReportTournamentsController(*self.tournaments)
@@ -191,6 +207,7 @@ class MainController(Controller):
         return new_state
 
     def _reports_players(self):
+        """REPORTS_PLAYERS: Report all players"""
         current_controller = self.current_controller
         if not isinstance(current_controller, ReportPlayersController):
             if self.current_tournament is not None:
@@ -203,6 +220,7 @@ class MainController(Controller):
         return new_state
 
     def _reports_menu(self):
+        """REPORTS_MENU: Prompt which report type the user desire to access"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mc.ReportsMenuController):
             current_controller = mc.ReportsMenuController()
@@ -211,6 +229,7 @@ class MainController(Controller):
         return new_state
 
     def _continue_tournament(self):
+        """CONTINUE_TOURNAMENT: Continue the current tournament from where it was last left"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mc.ContinueTournamentController):
             current_controller = mc.ContinueTournamentController()
@@ -220,6 +239,7 @@ class MainController(Controller):
         return new_state
 
     def _continue_tournament_menu(self):
+        """CONTINUE_TOURNAMENT_MENU: Prompt which tournament the user wants to continue"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.ContinueTournamentMenuController):
             current_controller = mec.ContinueTournamentMenuController(
@@ -230,6 +250,7 @@ class MainController(Controller):
         return new_state
 
     def _edit_match(self):
+        """EDIT_MATCH: Modify the current match"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditMatchController):
             current_controller = mec.EditMatchController()
@@ -241,6 +262,7 @@ class MainController(Controller):
         return new_state
 
     def _edit_match_menu(self):
+        """EDIT_MATCH_MENU: Prompt which match the user wants to edit"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditMatchMenuController):
             current_controller = mec.EditMatchMenuController(
@@ -250,10 +272,11 @@ class MainController(Controller):
         new_state, _ = current_controller.run()
         if new_state is not None and isinstance(current_controller.selected_item, Match):
             self.current_match = current_controller.selected_item
-            self.edited_data = self.current_match.copy()
+            self.edited_data = copy.copy(self.current_match)
         return new_state
 
     def _edit_round(self):
+        """EDIT_ROUND: Modify the current round"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditRoundController):
             current_controller = mec.EditRoundController()
@@ -265,6 +288,7 @@ class MainController(Controller):
         return new_state
 
     def _edit_round_menu(self):
+        """EDIT_ROUND_MENU: Prompt which round the user wants to edit"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditRoundMenuController):
             current_controller = mec.EditRoundMenuController(
@@ -274,10 +298,11 @@ class MainController(Controller):
         new_state, _ = current_controller.run()
         if new_state is not None and isinstance(current_controller.selected_item, Round):
             self.current_round = current_controller.selected_item
-            self.edited_data = self.current_round.copy()
+            self.edited_data = copy.copy(self.current_round)
         return new_state
 
     def _edit_tournament(self):
+        """EDIT_TOURNAMENT: Modify the current tournament"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditTournamentController):
             current_controller = mec.EditTournamentController()
@@ -289,6 +314,7 @@ class MainController(Controller):
         return new_state
 
     def _edit_tournament_menu(self):
+        """EDIT_TOURNAMENT_MENU: Prompt which tournament the user wants to edit"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditTournamentMenuController):
             current_controller = mec.EditTournamentMenuController(*self.tournaments)
@@ -296,16 +322,18 @@ class MainController(Controller):
         new_state, _ = current_controller.run()
         if new_state is not None and isinstance(current_controller.selected_item, Tournament):
             self.current_tournament = current_controller.selected_item
-            self.edited_data = self.current_tournament.copy()
+            self.edited_data = copy.copy(self.current_tournament)
         return new_state
 
     def _new_tournament(self):
+        """NEW_TOURNAMENT: Create a new tournament and redirect to EDIT_TOURNAMENT"""
         self.current_tournament = None
         self.edited_data = Tournament()
         self.states.pop()
         return MainViewState.EDIT_TOURNAMENT
 
     def _tournament_menu(self):
+        """TOURNAMENT_MENU: Tournament menu prompting the user to create or edit a tournament"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mc.TournamentsMenuController):
             current_controller = mc.TournamentsMenuController()
@@ -314,6 +342,7 @@ class MainController(Controller):
         return new_state
 
     def _edit_player(self):
+        """EDIT_PLAYER: Prompt the user for modifications to the player"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditPlayerController):
             current_controller = mec.EditPlayerController()
@@ -325,6 +354,7 @@ class MainController(Controller):
         return new_state
 
     def _edit_player_menu(self):
+        """EDIT_PLAYER_MENU: Prompt the user which player to edit"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mec.EditPlayerMenuController):
             current_controller = mec.EditPlayerMenuController(*self.players)
@@ -332,16 +362,18 @@ class MainController(Controller):
         new_state, _ = current_controller.run()
         if new_state is not None and isinstance(current_controller.selected_item, Player):
             self.current_player = current_controller.selected_item
-            self.edited_data = self.current_player.copy()
+            self.edited_data = copy.copy(self.current_player)
         return new_state
 
     def _new_player(self):
+        """NEW_PLAYER: Create a new player and redirect to EDIT_PLAYER"""
         self.current_player = None
         self.edited_data = Player()
         self.states.pop()
         return MainViewState.EDIT_PLAYER
 
     def _players_menu(self):
+        """PLAYERS_MENU: Menu prompting user to create a new user or edit one"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mc.PlayersMenuController):
             current_controller = mc.PlayersMenuController()
@@ -350,6 +382,7 @@ class MainController(Controller):
         return new_state
 
     def _main_menu(self):
+        """MAIN_MENU: Promp user with the main menu content"""
         current_controller = self.current_controller
         if not isinstance(current_controller, mc.MainMenuController):
             current_controller = mc.MainMenuController()
@@ -377,6 +410,7 @@ MENU_RESETS = [
     MainViewState.REPORTS_ROUNDS_MENU,
     # MainViewState.REPORTS_MATCHS_MENU,
 ]
+"""List of all states that shall redisplay their menu when swithching back to them"""
 MAPPED_STATE_METHODS = {
     MainViewState.MAIN_MENU: MainController._main_menu,
     MainViewState.PLAYERS_MENU: MainController._players_menu,
@@ -397,7 +431,7 @@ MAPPED_STATE_METHODS = {
     MainViewState.REPORTS_PLAYERS: MainController._reports_players,
     MainViewState.REPORTS_TOURNAMENTS_MENU: MainController._reports_tournaments_menu,
     MainViewState.REPORTS_ROUNDS_MENU: MainController._reports_rounds_menu,
-    MainViewState.REPORTS_MATCHS_MENU: MainController._reports_match_menu,
+    MainViewState.REPORTS_MATCHS_MENU: MainController._reports_matchs_menu,
     MainViewState.EDIT_FIELD: MainController._edit_field,
     MainViewState.SAVE_ITEM: MainController._save_item,
     MainViewState.BACK: MainController._back,
@@ -409,3 +443,4 @@ MAPPED_STATE_METHODS = {
     # case MainViewState.SET_MATCH_SCORE:
     # case MainViewState.EDIT_TOURNAMENT_STYLE:
 }
+"""Map all available consollers with their method counterpart"""
